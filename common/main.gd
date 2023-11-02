8d648bb2064a2e9edb6b0ae828835524c3829bf6
Sesso Kosga <kosgasesso@gmail.com>
2023-11-02 11:53:52 +0100

    feat: Added hero selection on deck editing screen

--- common/main.gd
+++ common/main.gd
@@ -1,5 +1,12 @@
 extends Control
 
+var by_pass_menu = true
+
+func _ready() -> void:
+	if by_pass_menu:
+		_on_my_collection_pressed()
+	pass
+
 func _on_language_option_item_selected(index:int) -> void:
 	match index:
 		0:
@@ -14,7 +21,7 @@ func _on_language_option_item_selected(index:int) -> void:
 
 
 func _on_my_collection_pressed() -> void:
-	#get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://common/my_collection.tscn")
 	pass # Replace with function body.
 
 func _on_start_pressed() -> void:
--- common/my_collection.gd
+++ common/my_collection.gd
@@ -1,1 +1,57 @@
+@tool
 extends Control
+
+@onready var hero_choice_container := $"%HeroChoiceContainer"
+@onready var selected_card_name := $"%SelectedCardName"
+@onready var selected_card_texture : TextureRect= $"%SelectedCardTexture"
+@onready var btn_next : Button= $"%Next"
+@onready var hero_choice : Control= $"%HeroChoice"
+@onready var cards_choice : Control= $"%CardsChoice"
+
+func filter_hero(item)->bool:
+	return item.type == CardData.Warlock.Type.Hero
+
+func _ready() -> void:
+	for row  in CardData.table_warlock.all.filter(filter_hero):
+		var card = Card.get_instance()
+		card.mode = Card.Mode.Field
+		card.title = row.title
+		card.id = row.id
+		card.profile = row.profile
+		card.hide_attack()
+		card.hide_health()
+		var label = Label.new()
+		label.text = card.title
+		label.position.y -= 20
+		card.add_child(label)
+		hero_choice_container.add_child(card)
+	pass
+
+func _process(_delta: float) -> void:
+	# for card:Card in hero_choice_container.get_children():
+	# 	if card.mode != Card.Mode.Field:
+	# 		card.mode = Card.Mode.Field
+			# print(card.title)
+			# get_node( str(card.get_path()) + "/Name" ).text = card.title
+	pass
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		for card :Card in hero_choice_container.get_children():
+			var surf = Rect2(card.global_position,card.size)
+			if surf.has_point(event.position):
+				selected_card_name.text = card.title
+				selected_card_texture.texture = card.get_profile_texture()
+				btn_next.disabled = false
+		
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://common/main.tscn")
+
+
+
+func _on_next_pressed() -> void:
+	hero_choice.hide()
+	cards_choice.show()
+	pass # Replace with function body.
